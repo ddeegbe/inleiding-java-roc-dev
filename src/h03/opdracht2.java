8e02d9727e0595d399afb853ed4d629b79c93bb6
damian <ddeegbe@roc-dev.com>
2019-11-13 10:00:22 +0100

    T/m 2.1

---
+++ src/h03/opdracht2.java
@@ -0,0 +1,19 @@
+
+
+import java.awt.*;
+import java.applet.*;
+
+public class opdracht2 extends Applet {
+    // Override init()
+    public void init() {
+        setBackground(Color.white);
+    }
+
+
+    public void paint(Graphics g) {
+        setBackground(Color.blue;
+        g.setColor(red.white,blue);
+        g.drawRect(20, 20, 100, 50);
+        g.fillRect(20, 80, 100, 50);
+    }
+}
